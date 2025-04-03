6c470b7791fcbe2475877b3de253d7021ab5a266
Uriel-PS <uriel.psrpg@gmail.com>
2025-04-03 07:37:39 -0300

    Adjustments and primitive leave the curve case

--- scripts/PID.ino
+++ scripts/PID.ino
@@ -1,56 +1,58 @@
-#include <BluetoothSerial.h>;
+#include <BluetoothSerial.h>
 
 BluetoothSerial SerialBT;
 
 #define PWMA 4 
-#define AIN2 17
-#define AIN1 16
+#define AIN2 16
+#define AIN1 17
 #define STBY 5
 #define BIN1 18
 #define BIN2 19
 #define PWMB 21
 
-const unsigned int sensor[] = {27, 26, 25, 33, 32, 35};
-const unsigned int sensorLat[] = {13, 14, 34, 39};
+const unsigned int sensor[] = {35, 32, 33, 25, 26, 27};
+const unsigned int sensorLat[] = {13, 39};
 const int n_sensores = 6;
-int pesos[] = {-3, -2, -1, 1, 2, 3};
+int pesos[] = {3, 2, 1, -1, -2, -3};
 
 //speeds
 int rspeed;
 int lspeed;
-const int base_speed = 120;
+const int base_speed = 80;
 
 int pos = 0;
 int sensor_read[n_sensores];
 long sensor_average = 0;
 int sensor_sum = 0;
+int r_read=0;
+int l_read=0;
+int aux = 0, atual = 0;
 
 float p;
 float integral;
-float integral_max = 1000;
-float integral_min = -1000;
+float integral_max = 5000;
+float integral_min = -5000;
 float d;
 float lp;
 float error;
 float correction;
 float sp;
 
-float Kp = 0.8;
-float Ki = 0;
-float Kd = 1;
-
+float Kp = 2.4;
+float Ki = 0.002;
+float Kd = 7;
+int state = 0;
 int pid_calc();
 void calc_turn();
-void motor_drive(int , int );
 
 void setup()
 {
-  SerialBT.begin("LineFollower");
+  SerialBT.begin("Hermes");
   //sensors
   for(int i=0; i<n_sensores; i++){
     pinMode(sensor[i], INPUT);
   }
-
+  atual = 0;
   //motors
   pinMode(STBY, OUTPUT);
   pinMode(PWMA, OUTPUT);
@@ -71,6 +73,10 @@ void setup()
   Serial.begin(115200);
   
   sp = 0;
+  while(!SerialBT.available()) {delay(10);}
+  String input = SerialBT.readString();  // Read the incoming data as a string
+  updatePIDConstants(input); 
+
   analogWrite(PWMA, base_speed);
   analogWrite(PWMB, base_speed);
 }
@@ -78,15 +84,99 @@ void setup()
 
 void loop()
 {
-  atual = micros();
-  Serial.println(atual-anterior);
-  anterior = atual;
+  
   if (SerialBT.available()) {  // Check if Bluetooth data is available
     String input = SerialBT.readString();  // Read the incoming data as a string
     updatePIDConstants(input);  // Function to parse and update Kp, Ki, Kd
   }
-  calc_turn();
-  delay(5);
+  
+  /*
+  Serial.print("state: ");
+  Serial.print(state);
+  Serial.print("\taux: ");
+  Serial.print(aux);
+  Serial.print("\tesquerdo: ");
+  Serial.print(analogRead(13));
+  Serial.print("\tdireito: ");
+  Serial.print(analogRead(39));
+  Serial.print("\tatual: ");
+  Serial.println(atual);
+ */
+  //boolean value
+  //1 if reading white
+  //0 if reading black
+  r_read = analogRead(39) <= 2000;
+  l_read = analogRead(13) <= 1000;
+  switch (state){
+    case 0:
+      calc_turn();
+      
+      if(r_read){
+        state++;
+      }
+      
+
+      break;
+    case 1:
+      calc_turn();
+
+      if (!r_read)
+        state++;
+
+      break;
+    case 2:
+      calc_turn();
+      
+      
+        if (r_read || l_read){//se o da direita ler branco
+    
+          if(atual == 0){//se for a primeira vez que o da direita leu branco, salva como referencia para subtrair depois
+            atual = millis();
+          }
+        }
+
+      if(atual != 0){
+        if (millis() - atual >= 300){
+          if(aux == 0){
+
+          atual = 0;
+          state ++;
+          }else{
+
+          aux = 0;
+          atual = 0;
+          }
+        }else{
+
+          if(l_read){//se o da esquerda ler branco, salva isso
+            aux = 1;
+          }
+        }
+      }
+
+      break;
+    case 3:
+      if (!r_read)
+        state++;
+      break;
+    case 4:
+      analogWrite(PWMA, 0);
+      analogWrite(PWMB, 0);
+      while(!SerialBT.available()) {delay(10);}
+      String input = SerialBT.readString();  // Read the incoming data as a string
+      updatePIDConstants(input);
+      state=0;
+  }
+
+//leave the curve
+  if(analogRead(35) >= 2000 && analogRead(32) >= 2000 && analogRead(33) >= 2000 && analogRead(26) >= 2000 && analogRead(25) >= 2000 && analogRead(27) >= 2000){
+    while (analogRead(33) >= 2000 && analogRead(25) >= 2000){
+      analogWrite(PWMA, 0);
+      analogWrite(PWMB, 100);
+    }
+  }
+  
+  delay(10);
 }
 
 int pid_calc()
@@ -94,10 +184,12 @@ int pid_calc()
   sensor_average = 0;
   sensor_sum = 0;
 
-  for(int i = 0; i < 6; i++)
+  
+
+  for(int i = 0; i < n_sensores; i++)
   {
     sensor_read[i]=analogRead(sensor[i]);
-    sensor_average += sensor_read[i]*pesos[i]*1000;
+    sensor_average += sensor_read[i]*pesos[i]*100;
     sensor_sum += sensor_read[i];
   }
 
@@ -109,7 +201,7 @@ int pid_calc()
   integral = constrain(integral, integral_min, integral_max);
   d = p - lp;
   lp = p;
-
+  
   return  int(Kp*p + Ki*integral + Kd*d);
 }
 
